bfd71ddbb2761b436aa8c29693099f07ed3bc539
Weiyuan Liu <weiyuan.lane@gmail.com>
2021-02-04 23:58:21 +0800

    [Ops] First commit

---
+++ internal/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"os"
+)
+
+var (
+	port = os.Getenv("PORT")
+)
+
+func main() {
+	// http.HandleFunc("/hello", hello)
+	// http.HandleFunc("/headers", headers)
+
+	// http.ListenAndServe(port, nil)
+}
